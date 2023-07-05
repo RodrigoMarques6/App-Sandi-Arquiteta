bed9496dd2081439b18df3df3d66ba4e79a5886d
RodrigoMarques <rodrigomarques328@hotmail.com>
2023-07-05 18:35:09 -0300

    App-Sandi-Arquiteta

--- src/app/components/tabela-economico/tabela-economico.component.ts
+++ src/app/components/tabela-economico/tabela-economico.component.ts
@@ -6,19 +6,21 @@ export interface PeriodicElement {
   modalidade: number;
   desconto: number;
   receba_total: number;
+  receba_mes: number;
+  cliente_paga_mes: number,
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {modalidade: 1, taxa: 'Hydrogen', desconto: 1.0079, receba_total: 1000},
-  {modalidade: 2, taxa: 'Helium', desconto: 4.0026, receba_total: 1000},
-  {modalidade: 3, taxa: 'Lithium', desconto: 6.941, receba_total: 1000},
-  {modalidade: 4, taxa: 'Beryllium', desconto: 9.0122, receba_total: 1000},
-  {modalidade: 5, taxa: 'Boron', desconto: 10.811, receba_total: 1000},
-  {modalidade: 6, taxa: 'Carbon', desconto: 12.0107, receba_total: 1000},
-  {modalidade: 7, taxa: 'Nitrogen', desconto: 14.0067, receba_total: 1000},
-  {modalidade: 8, taxa: 'Oxygen', desconto: 15.9994, receba_total: 1000},
-  {modalidade: 9, taxa: 'Fluorine', desconto: 18.9984, receba_total: 1000},
-  {modalidade: 10, taxa: 'Neon', desconto: 20.1797, receba_total: 1000},
+  {modalidade: 1, taxa: 'Hydrogen', desconto: 1.0079, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
+  {modalidade: 2, taxa: 'Helium', desconto: 4.0026, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
+  {modalidade: 3, taxa: 'Lithium', desconto: 6.941, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
+  {modalidade: 4, taxa: 'Beryllium', desconto: 9.0122, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
+  {modalidade: 5, taxa: 'Boron', desconto: 10.811, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
+  {modalidade: 6, taxa: 'Carbon', desconto: 12.0107, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
+  {modalidade: 7, taxa: 'Nitrogen', desconto: 14.0067, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
+  {modalidade: 8, taxa: 'Oxygen', desconto: 15.9994, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
+  {modalidade: 9, taxa: 'Fluorine', desconto: 18.9984, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
+  {modalidade: 10, taxa: 'Neon', desconto: 20.1797, receba_total: 1000, receba_mes: 1000, cliente_paga_mes: 1000},
 ];
 
 @Component({
@@ -29,6 +31,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
   imports: [MatTableModule],
 })
 export class TabelaEconomicoComponent {
-  displayedColumns: string[] = ['modalidade', 'taxa', 'desconto', 'receba_total'];
+  displayedColumns: string[] = ['modalidade', 'taxa', 'desconto', 'receba_total', 'receba_mes', 'cliente_paga_mes'];
   dataSource = ELEMENT_DATA;
 }
