45ed2124834edbd400b9374b8e9584c4a757a235
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2023-07-05 22:09:57 -0300

    App-Sandi-Arquiteta

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+
+import { MenuComponent } from "./components/menu/menu.component";
+import { PlanoEconomicoComponent } from "./components/plano-economico/plano-economico.component";
+import { TabelaEconomicoComponent } from "./components/tabela-economico/tabela-economico.component";
+import { HomeComponent } from "./components/home/home.component";
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'tabela', component: TabelaEconomicoComponent},
+];
+
+@NgModule({
+    declarations: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { PlanoEconomicoComponent } from './components/plano-economico/plano-economico.component';
 import { TabelaEconomicoComponent } from './components/tabela-economico/tabela-economico.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlanoEconomicoComponent,
+    HomeComponent,
     
   ],
   imports: [
     BrowserModule,
     MenuComponent,
     TabelaEconomicoComponent,
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
