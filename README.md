932b6322c245df4124f7d016d7c6d9bfcff95755
RodrigoMarques6 <rodrigomarques328@hotmail.com>
2023-07-07 20:06:02 -0300

    App-Sandi-Arquiteta

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # AppSandi
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.3.
 
 ## Development server
 
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
-import { MatExpansionModule } from '@angular/material/expansion';
+import {MatExpansionModule} from '@angular/material/expansion';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [
-    MatExpansionModule,
-  ]
+  imports: [MatExpansionModule]
 })
 export class HomeComponent {
-  panelOpenState = false;
+
 }
